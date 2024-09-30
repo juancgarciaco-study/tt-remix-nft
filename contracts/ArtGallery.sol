ee5d1ebc80168c41b09fec247c3cdae7c56ffd1c
juancgarciaco-study <juancgarciaco-study@outlook.com>
2024-09-30 13:10:26 -0500

    Document ArtGallery.sol

--- contracts/ArtGallery.sol
+++ contracts/ArtGallery.sol
@@ -1,10 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "hardhat/console.sol";
+
+/*
+A brief explanation of the Solidity code:
+
+This is a smart contract for an art gallery, allowing artists to mint and sell their artwork. The contract uses OpenZeppelin's ERC721 and Ownable contracts for token management and access control.
+
+The contract has two structs: `Artist` and `Gallery`, which represent the artist and gallery wallets, royalty percentage, and token name.
+
+The contract has three main functions:
+
+1. `mintArtwork`: allows the owner to mint a new artwork, setting the artist, gallery, and token name.
+2. `buyArtwork`: allows anyone to buy an artwork, transferring the ownership and paying the artist, gallery, and project creators their respective royalties.
+*/
 
 contract ArtGallery is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
@@ -18,7 +31,7 @@ contract ArtGallery is ERC721URIStorage, Ownable {
 
     struct Gallery {
         address payable wallet;
-        uint256 commissionPercentage;
+        uint256 royaltyPercentage;
     }
 
     mapping(uint256 => Artist) public artists;
@@ -31,22 +44,30 @@ contract ArtGallery is ERC721URIStorage, Ownable {
         ERC721("ArtGallery", "ART")
         Ownable(initialOwner)
     {
-        console.log("ArtGallery.ctor -> initialOwner", initialOwner);
+        // console.log("ArtGallery.ctor -> initialOwner", initialOwner);
     }
 
+    /*
+     * @notice Mint a new artwork.
+     * @param artistWallet The wallet of the artist.
+     * @param artistRoyaltyPercentage The royalty percentage of the artist.
+     * @param galleryWallet The wallet of the gallery.
+     * @param galleyRoyaltyPercentage The royalty percentage of the gallery.
+     * @param tokenURI The URI of the artwork.
+     * @param tokenName The name of the artwork.
+     * @return The ID of the minted artwork.
+     */
     function mintArtwork(
         address artistWallet,
-        uint256 royaltyPercentage,
+        uint256 artistRoyaltyPercentage,
         address galleryWallet,
-        uint256 commissionPercentage,
+        uint256 galleyRoyaltyPercentage,
         string memory tokenURI,
         string memory tokenName
     ) public onlyOwner returns (uint256) {
-        console.log("ArtGallery.mintArtwork -> msg.sender", msg.sender);
+        // console.log("ArtGallery.mintArtwork -> msg.sender", msg.sender);
         require(
-            royaltyPercentage +
-                commissionPercentage +
-                projectCommissionPercentage <=
+            artistRoyaltyPercentage + galleyRoyaltyPercentage + projectCommissionPercentage <=
                 50,
             "Total percentage exceeds 50 percent"
         );
@@ -58,22 +79,21 @@ contract ArtGallery is ERC721URIStorage, Ownable {
 
         artists[newItemId] = Artist(
             payable(artistWallet),
-            royaltyPercentage,
+            artistRoyaltyPercentage,
             tokenName
         );
-        galleries[newItemId] = Gallery(
-            payable(galleryWallet),
-            commissionPercentage
-        );
+        galleries[newItemId] = Gallery(payable(galleryWallet), galleyRoyaltyPercentage);
 
         _transfer(ownerOf(newItemId), artistWallet, newItemId);
 
         return curToken = newItemId;
     }
 
+    /*
+     * @notice Buy an artwork.
+     * @param tokenId The ID of the artwork to buy.
+     */
     function buyArtwork(uint256 tokenId) public payable {
-        console.log("ArtGallery.buyArtwork -> msg.sender", msg.sender);
-        console.log("ArtGallery.buyArtwork -> msg.value", msg.value);
         require(_exists(tokenId), "Artwork does not exist");
         require(msg.value > 0, "Payment must be greater than 0");
 
@@ -82,8 +102,7 @@ contract ArtGallery is ERC721URIStorage, Ownable {
         Gallery memory gallery = galleries[tokenId];
 
         uint256 artistEarnings = (salePrice * artist.royaltyPercentage) / 100;
-        uint256 galleryEarnings = (salePrice * gallery.commissionPercentage) /
-            100;
+        uint256 galleryEarnings = (salePrice * gallery.royaltyPercentage) / 100;
         uint256 projectEarnings = (salePrice * projectCommissionPercentage) /
             100;
         uint256 sellerEarnings = salePrice -
@@ -99,6 +118,11 @@ contract ArtGallery is ERC721URIStorage, Ownable {
         _transfer(ownerOf(tokenId), msg.sender, tokenId);
     }
 
+    /*
+     * @notice Get the token name of an artwork.
+     * @param tokenId The ID of the artwork.
+     * @return The token name of the artwork.
+     */
     function getTokenName(uint256 tokenId) public view returns (string memory) {
         require(_exists(tokenId), "Token does not exist");
         return artists[tokenId].tokenName;
